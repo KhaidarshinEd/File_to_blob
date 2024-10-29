b6224d3e77d229b05e0914471626005e01c3acef
KhaidarshinER <khaidarshiner@yandex.ru>
2024-10-29 18:01:27 +0500

    первый коммит

---
+++ File_to_blob/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace File_to_blob
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSaveFile = new System.Windows.Forms.Button();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Путь к файлу";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // buttonSaveFile
+            // 
+            this.buttonSaveFile.Location = new System.Drawing.Point(135, 64);
+            this.buttonSaveFile.Name = "buttonSaveFile";
+            this.buttonSaveFile.Size = new System.Drawing.Size(106, 34);
+            this.buttonSaveFile.TabIndex = 2;
+            this.buttonSaveFile.Text = "Преобразовать";
+            this.buttonSaveFile.UseVisualStyleBackColor = true;
+            this.buttonSaveFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(12, 64);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(106, 34);
+            this.buttonOpenFile.TabIndex = 3;
+            this.buttonOpenFile.Text = "Выбрать файл";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(350, 19);
+            this.listBox1.TabIndex = 4;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "rezult";
+            this.saveFileDialog1.Filter = "Text files (*.txt)|*.txt|SQL files (*.sql)|*.txt|All files (*.*)|*.*";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(256, 64);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(106, 34);
+            this.buttonClose.TabIndex = 5;
+            this.buttonClose.Text = "Закрыть";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 126);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Controls.Add(this.buttonSaveFile);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button buttonSaveFile;
+        private Button buttonOpenFile;
+        private OpenFileDialog openFileDialog1;
+        private ListBox listBox1;
+        private SaveFileDialog saveFileDialog1;
+        private Button buttonClose;
+    }
+}
---
+++ File_to_blob/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.IO;
+using System.Text;
+using System.Drawing;
+
+namespace File_to_blob
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fileName = @openFileDialog1.FileName;   //открываем выбранный файл
+            var fileIn = File.ReadAllBytes(fileName);   //заносим в переменную выбранный файл
+            string s = Convert.ToBase64String(fileIn);  //конвертируем в base64
+            
+
+            //Отображаем окно выбора файла
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // переменная для удаления файла
+                FileInfo fileInf = new FileInfo(saveFileDialog1.FileName);
+                //если файл уже существовал, то удаляем его
+                fileInf.Delete();
+
+                //переменная для записи данных в файл
+                StreamWriter fileWrite = new StreamWriter(fileInf.Create());
+
+                FileInfo fileInf2 = new FileInfo(openFileDialog1.FileName);
+                //запись данных в файл
+                //fileWrite.WriteLine("Тестовая запись в файл");
+                //fileWrite.WriteLine(openFileDialog1.FileName);
+                //fileWrite.WriteLine(s);   // заносим конвертированную
+                //fileWrite.WriteLine(lenbase64);
+
+                //СОЗДАЮ СКРИПТ
+                var offset = 0;
+                var i = 1;
+                var step =0;
+                var lenbase64 = s.Length;
+                if (lenbase64 >= 2000) step = 2000; else step = lenbase64;
+                var param = s.Substring(offset, step);
+                var param2 = s.Substring(offset, lenbase64);
+
+              /*  fileWrite.WriteLine(fileInf2.Name);
+                fileWrite.WriteLine("Длинна: " + lenbase64);
+                fileWrite.WriteLine("Для проверки: " + s);
+                */fileWrite.WriteLine("declare");
+                while (lenbase64 > offset)
+                {
+                    if((lenbase64-offset) < step) step = lenbase64 - offset;
+                    param = s.Substring(offset, step);
+                    fileWrite.WriteLine(string.Format("v_blob_base{1} varchar2({2}) := '{0}';", param, i, step));
+                    offset += step;
+                    i++;
+
+                }
+                fileWrite.WriteLine("v_blob_data   blob;");
+                fileWrite.WriteLine();
+                fileWrite.WriteLine("function from_base64(t in varchar2) return varchar2 is");
+                fileWrite.WriteLine("begin");
+                fileWrite.WriteLine(" return utl_encode.base64_decode(utl_raw.cast_to_raw(t));");
+                fileWrite.WriteLine("end from_base64;") ;
+                fileWrite.WriteLine();
+                fileWrite.WriteLine("procedure writeBlobChunk(p_data in varchar2) is");
+                fileWrite.WriteLine("chnk raw(32767) := HEXTORAW(from_base64(p_data));");
+                fileWrite.WriteLine("begin");
+                fileWrite.WriteLine(" dbms_lob.writeappend(v_blob_data, utl_raw.length(chnk), chnk);");
+                fileWrite.WriteLine("end;");
+                fileWrite.WriteLine();
+                fileWrite.WriteLine("begin");
+                fileWrite.WriteLine(string.Format("insert into lob_table(bdy, name_nm)  values(empty_blob(), '{0}')  returning bdy into v_blob_data;", fileInf2.Name));
+                fileWrite.WriteLine();
+
+                for (int j=1; j < i; j++)
+                { 
+                    fileWrite.WriteLine(string.Format("writeBlobChunk(v_blob_base{0});",j)); 
+                }
+                fileWrite.WriteLine("commit;");
+                fileWrite.WriteLine("end;");
+                fileWrite.WriteLine("/");
+
+                //закрываем файл
+                fileWrite.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+            {
+
+            //Отображаем окно выбора файла
+            if (openFileDialog1.ShowDialog() == DialogResult.OK && openFileDialog1.FileName != null)
+            { 
+                //Создаем переменную для чтения из файла
+                StreamReader Fileread = new StreamReader(openFileDialog1.FileName);
+
+                listBox1.Items.Add(openFileDialog1.FileName);
+                Fileread.Close();
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
